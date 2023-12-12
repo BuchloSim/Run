70342d9cb70a79383084bc0fb18632bec70ffa99
BuchloSim <117743008+BuchloSim@users.noreply.github.com>
2023-12-12 12:48:15 +0100

    Vložení složky s projektem

---
+++ Run/src/js/script.js
@@ -0,0 +1,15 @@
+$('.scooters-slider').slick({
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    prevArrow: ".scooters-prev",
+    nextArrow: ".scooters-next",
+  });
+
+$('.reviews-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: ".reviews-prev",
+    nextArrow: ".reviews-next",
+});
